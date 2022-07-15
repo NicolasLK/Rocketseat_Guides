d2b979379990e18abe52e27bfd622bbc9da55033
Nicolas L. Kaminski <nicolas.kaminski.2001@gmail.com>
2022-07-15 09:35:56 -0300

    Iniciando Ignite

---
+++ Guia_Reactjs/Ignite/ReactJS/01_GitHub_Explorer/src/components/RepositoryItem.jsx
@@ -0,0 +1,17 @@
+const repositoryName = "Unform"
+
+export function RepositoryItem() {
+    return (
+
+    <li>
+        <strong>{repositoryName}</strong>
+        <p>Forms in React</p>
+
+        <a href="">
+            Acessar repositório
+        </a>
+    </li>
+
+
+    )
+}
---
+++ Guia_Reactjs/Ignite/ReactJS/01_GitHub_Explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,14 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+                <RepositoryList />
+                <RepositoryList />
+                <RepositoryList />
+                <RepositoryList />
+            </ul>
+        </section>
+    );
+}

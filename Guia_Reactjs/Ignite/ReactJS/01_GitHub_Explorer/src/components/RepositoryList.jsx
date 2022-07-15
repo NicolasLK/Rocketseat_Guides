b3ce9cc6acf17982736eef3dd4d6fa0e214eaf90
Nicolas L. Kaminski <nicolas.kaminski.2001@gmail.com>
2022-07-15 11:39:57 -0300

    Chamada da API finalizada

--- Guia_Reactjs/Ignite/ReactJS/01_GitHub_Explorer/src/components/RepositoryList.jsx
+++ Guia_Reactjs/Ignite/ReactJS/01_GitHub_Explorer/src/components/RepositoryList.jsx
@@ -1,21 +1,31 @@
 import { RepositoryItem } from "./RepositoryItem";
+import "../styles/repositories.scss"
+import { useState, useEffect } from "react";
 
-const repository = {
-    name: "unform",
-    description: "Forms in React",
-    link: "https://github.com/unform/unform"
-}
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([])
+    const url = "https://api.github.com/orgs/rocketseat/repos"
+
+    useEffect(() => {
+        try {
+            fetch(url)
+            .then(res => res.json())
+            .then(data => setRepositories(data))
+        } catch (e) {
+            console.log(e)
+        }
+    }, [])
+
+
     return (
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     );

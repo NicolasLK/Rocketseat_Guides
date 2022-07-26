ebbf539912afa45ce4b70086884b323d58bfafc8
Nicolas L. Kaminski <nicolas.kaminski.2001@gmail.com>
2022-07-26 12:01:56 -0300

    NextJS With Stripe and FaunaDB (Precisa de ajustes)

--- Guia_Reactjs/Ignite/NextJS/ignews/src/components/SubscribeButton/index.tsx
+++ Guia_Reactjs/Ignite/NextJS/ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from "next-auth/react"
+import { api } from "../../services/api"
+import { getStripeJs } from "../../services/stripe-js"
 import styles from "./styles.module.scss"
 
 interface SubscribeButtonProps {
@@ -5,10 +8,35 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const { data: session } = useSession()
+
+    async function handleSubscribeButton() {
+        if (!session) {
+            signIn('github')
+            return
+        }
+
+        try {
+            const response = await api.post('/subscribe', {
+                user: session.user
+            })
+
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({ sessionId })
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribeButton}
         >
             Subscribe Now
         </button>
